0796f33935d142235bfa9cb590a2ee879e11a20f
filipenadai <f.nadai05@gmail.com>
2021-10-02 12:27:33 -0300

    add subscribe button and stripe

---
+++ react/ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+
+interface SubscribeProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeProps) {
+  return (
+
+    <button
+      type="button"
+      className={styles.subscribeButton}
+    >
+      Subscribe Now
+    </button>
+  );
+}
