2621695360f00ae0ae77158c832401fde69ec702
filipenadai <f.nadai05@gmail.com>
2021-10-17 22:58:59 -0300

    init signin screen

--- react/dashgo/src/pages/index.tsx
+++ react/dashgo/src/pages/index.tsx
@@ -1,5 +1,48 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex h="100vh" w="100vw" justify="center" align="center">
+      <Flex as="form" w="100%" maxW={360} bg="gray.800" p="8" flexDir="column" borderRadius="8">
+        <Stack spacing="4">
+
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+
+            <Input
+              name="email"
+              type="email"
+              id="email"
+              focusBorderColor="pink.500"
+              bg="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bg="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button size="lg" type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
