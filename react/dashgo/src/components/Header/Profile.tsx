60dbf9dff9662d12275fd2a1b21e82691ef42036
filipenadai <f.nadai05@gmail.com>
2021-11-02 10:18:24 -0300

    refact components

--- react/dashgo/src/components/Header/Profile.tsx
+++ react/dashgo/src/components/Header/Profile.tsx
@@ -1,17 +1,21 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react';
 
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
-export function Profile() {
+export function Profile({ showProfileData = true  }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Filipe Nadai</Text>
-        <Text color="gray.300" fontSize="small">
-          filipe@nadai.dev
-        </Text>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Filipe Nadai</Text>
+          <Text color="gray.300" fontSize="small">
+            filipe@nadai.dev
+          </Text>
 
-      </Box>
-
+        </Box>
+      )}
       <Avatar size="md" name="Filipe Nadai" src="https://github.com/filipenadai.png" />
     </Flex>
   )
