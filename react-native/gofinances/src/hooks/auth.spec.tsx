d53eed6bb4142fa1abe959045d64a8162d8ace82
filipenadai <f.nadai05@gmail.com>
2021-12-31 19:14:38 -0300

    add mocks in tests

--- react-native/gofinances/src/hooks/auth.spec.tsx
+++ react-native/gofinances/src/hooks/auth.spec.tsx
@@ -1,5 +1,7 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import fetchMock from 'jest-fetch-mock';
+import { mocked } from 'jest-mock';
+import { startAsync } from 'expo-auth-session';
 
 import { AuthProvider, useAuth } from './auth';
 
@@ -12,19 +14,17 @@ const userTest = {
   photo: 'any_photo.png'
 };
 
-jest.mock('expo-auth-session', () => {
-  return {
-    startAsync: () => ({
+jest.mock('expo-auth-session')
+
+describe('Auth Hook', () => {
+  it('should be able to sign in with Google account existing', async () => {
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValue({
       type: 'success',
       params: {
         access_token: 'any_token',
       }
-    }),
-  }
-})
-
-describe('Auth Hook', () => {
-  it('should be able to sign in with Google account existing', async () => {
+    })
     fetchMock.mockResponseOnce(JSON.stringify(userTest));
 
     const { result } = renderHook(() => useAuth(), {
@@ -35,5 +35,25 @@ describe('Auth Hook', () => {
 
     expect(result.current.user.email)
       .toBe(userTest.email);
+  });
+
+  it('should be not able to sign if user cancel signin', async () => {
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValue({
+      type: 'cancel',
+      params: {
+        access_token: 'any_token',
+      }
+    })
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user)
+      .not
+      .toHaveProperty('id')
   });
 });
